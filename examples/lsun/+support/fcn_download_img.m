8c554addf1a35ada498588cd622504466883ce04
shodimaggio <shogo@eng.niigata-u.ac.jp>
2022-05-07 18:06:49 +0900

    Added sample scripts

---
+++ examples/lsun/+support/fcn_download_img.m
@@ -0,0 +1,37 @@
+function fcn_download_img(isVerbose)
+% FCN_DOWNLOAD_IMG
+%
+% Copyright (c) Shogo MURAMATSU, 2018-2022
+% All rights reserved.
+%
+
+% デフォルトの値を設定
+if nargin < 1
+    isVerbose = true;
+end
+
+% 画像サンプルのダウンロード
+dstdir = './data/';
+if exist(dstdir,'dir') ~= 7
+    mkdir(dstdir)
+end
+%fnames = {'lena' 'baboon' 'goldhill' 'barbara'};
+for idx = 1:24 %length(fnames)
+    fname = "kodim"+num2str(idx,'%02d') + ".png"; % [ fnames{idx} '.png' ]
+    if exist(fullfile(dstdir,fname),'file') ~= 2
+        img = imread(...
+            ...sprintf('http://homepages.cae.wisc.edu/~ece533/images/%s',...
+            ...fname));
+            "http://www.r0k.us/graphics/kodak/kodak/"+fname);
+        imwrite(img,fullfile(dstdir,fname))
+        if isVerbose
+            fprintf('Downloaded and saved %s in %s\n',fname,dstdir);
+        end
+    else
+        if isVerbose
+            fprintf('%s already exists in %s\n',fname,dstdir);
+        end
+    end
+end
+disp('See <a href="http://www.r0k.us/graphics/kodak/">Kodak Lossless True Color Image Suite</a>')
+
