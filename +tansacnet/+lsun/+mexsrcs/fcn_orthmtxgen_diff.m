f8f995922bcc4ecd413bc543f9319515bb137e20
shodimaggio <shogo@eng.niigata-u.ac.jp>
2022-05-08 21:02:21 +0900

    Revised GPU usage

--- +tansacnet/+lsun/+mexsrcs/fcn_orthmtxgen_diff.m
+++ +tansacnet/+lsun/+mexsrcs/fcn_orthmtxgen_diff.m
@@ -20,7 +20,7 @@ function [matrix,matrixpst,matrixpre] = fcn_orthmtxgen_diff(...
 %
 % http://msiplab.eng.niigata-u.ac.jp/
 
-if nargin < 7 
+if nargin < 7
     isLessThanR2021b = false;
 end
 if nargin < 6
@@ -33,71 +33,117 @@ end
 nDim_ = (1+sqrt(1+8.*size(angles,1)))/2;
 nMatrices_ = size(angles,2);
 matrix = repmat(eye(nDim_,'like',angles),[1 1 nMatrices_]);
-for iMtx = 1:nMatrices_
-    matrixrev = eye(nDim_,'like',angles);
-    matrixdif = zeros(nDim_,'like',angles);
+if useGpu
+    %for iMtx = 1:nMatrices_
+    matrixrev = repmat(eye(nDim_,'like',angles),[1 1 nMatrices_]);
+    matrixdif = zeros(nDim_,nDim_,nMatrices_,'like',angles);
     iAng = uint32(1);
     for iTop=1:nDim_-1
-        rt = matrixrev(iTop,:);
-        dt = zeros(1,nDim_,'like',angles);
-        dt(iTop) = 1;
+        rt = matrixrev(iTop,:,:);
+        dt = zeros(1,nDim_,nMatrices_,'like',angles);
+        dt(1,iTop,:) = 1;
         for iBtm=iTop+1:nDim_
             if iAng == pdAng
-                angle = angles(iAng,iMtx);
+                angle = angles(iAng,:); 
                 %
-                rb = matrixrev(iBtm,:);
-                db = zeros(1,nDim_,'like',angles);
-                db(iBtm) = 1;
+                rb = matrixrev(iBtm,:,:);
+                db = zeros(1,nDim_,nMatrices_,'like',angles);
+                db(1,iBtm,:) = 1;
                 dangle = angle + pi/2;
                 %
                 %[rt,rb] = rot_(rt,rb,-angle);
                 %[dt,db] = rot_(dt,db,dangle);
-                [vt,vb] = rot_([rt;dt],[rb;db],[-angle;dangle],...
-                    useGpu,isLessThanR2021b);
+                %[vt,vb] = rot_([rt;dt],[rb;db],[-angle;dangle],...
+                %    useGpu,isLessThanR2021b);
+                vt = [rt;dt];
+                vb = [rb;db];
+                angle_ = permute([-angle;dangle],[1 3 2]);
+                c = cos(angle_);
+                s = sin(angle_);
+                %u  = arrayfun(@(s,vt,vb) s.*(vt+vb),s,vt,vb);
+                %vt = arrayfun(@(c,s,vt,u) (c+s).*vt-u,c,s,vt,u);
+                %vb = arrayfun(@(c,s,vb,u) (c-s).*vb+u,c,s,vb,u);
+                u  = pagefun(@times,s,bsxfun(@plus,vt,vb));
+                vt = bsxfun(@minus,pagefun(@times,bsxfun(@plus,c,s),vt),u);
+                vb = bsxfun(@plus,pagefun(@times,bsxfun(@minus,c,s),vb),u);
                 %
-                matrixrev(iTop,:) = vt(1,:); %rt;
-                matrixrev(iBtm,:) = vb(1,:); %rb;
-                matrixdif(iTop,:) = vt(2,:); %dt;
-                matrixdif(iBtm,:) = vb(2,:); %db;
+                matrixrev(iTop,:,:) = vt(1,:,:); %rt;
+                matrixrev(iBtm,:,:) = vb(1,:,:); %rb;
+                matrixdif(iTop,:,:) = vt(2,:,:); %dt;
+                matrixdif(iBtm,:,:) = vb(2,:,:); %db;
                 %
-                matrixpst(:,:,iMtx) = matrixpst(:,:,iMtx)*matrixrev;
-                matrix(:,:,iMtx) = matrixpst(:,:,iMtx)*matrixdif*matrixpre(:,:,iMtx);
-                matrixpre(:,:,iMtx) = matrixrev.'*matrixpre(:,:,iMtx);
+                matrixpst = pagefun(@mtimes,matrixpst,matrixrev);
+                matrix = pagefun(@mtimes,pagefun(@mtimes,matrixpst,matrixdif),matrixpre);
+                matrixpre = pagefun(@mtimes,pagefun(@transpose,matrixrev),matrixpre);
             end
             iAng = iAng + 1;
         end
     end
+    %end
     if isvector(mus) || isscalar(mus)
-        if useGpu
-            matrix(:,:,iMtx) = arrayfun(@times,mus(:),matrix(:,:,iMtx));
-        elseif isLessThanR2021b % on CPU
-            matrix(:,:,iMtx) = bsxfun(@times,mus(:),matrix(:,:,iMtx));
-        else % on CPU
-            matrix(:,:,iMtx) = mus(:).*matrix(:,:,iMtx);
-        end
+        matrix = pagefun(@times,mus(:),matrix);
     else
-        if useGpu
-            matrix(:,:,iMtx) = arrayfun(@times,mus(:,iMtx),matrix(:,:,iMtx));
-        elseif isLessThanR2021b % on CPU
-            matrix(:,:,iMtx) = bsxfun(@times,mus(:,iMtx),matrix(:,:,iMtx));
-        else % on CPU
-            matrix(:,:,iMtx) = mus(:,iMtx).*matrix(:,:,iMtx);
+        matrix = pagefun(@times,permute(mus,[1 3 2]),matrix);
+    end
+else
+    for iMtx = 1:nMatrices_
+        matrixrev = eye(nDim_,'like',angles);
+        matrixdif = zeros(nDim_,'like',angles);
+        iAng = uint32(1);
+        for iTop=1:nDim_-1
+            rt = matrixrev(iTop,:);
+            dt = zeros(1,nDim_,'like',angles);
+            dt(iTop) = 1;
+            for iBtm=iTop+1:nDim_
+                if iAng == pdAng
+                    angle = angles(iAng,iMtx);
+                    %
+                    rb = matrixrev(iBtm,:);
+                    db = zeros(1,nDim_,'like',angles);
+                    db(iBtm) = 1;
+                    dangle = angle + pi/2;
+                    %
+                    %[rt,rb] = rot_(rt,rb,-angle);
+                    %[dt,db] = rot_(dt,db,dangle);
+                    [vt,vb] = rot_([rt;dt],[rb;db],[-angle;dangle],...
+                        isLessThanR2021b);
+                    %
+                    matrixrev(iTop,:) = vt(1,:); %rt;
+                    matrixrev(iBtm,:) = vb(1,:); %rb;
+                    matrixdif(iTop,:) = vt(2,:); %dt;
+                    matrixdif(iBtm,:) = vb(2,:); %db;
+                    %
+                    matrixpst(:,:,iMtx) = matrixpst(:,:,iMtx)*matrixrev;
+                    matrix(:,:,iMtx) = matrixpst(:,:,iMtx)*matrixdif*matrixpre(:,:,iMtx);
+                    matrixpre(:,:,iMtx) = matrixrev.'*matrixpre(:,:,iMtx);
+                end
+                iAng = iAng + 1;
+            end
+        end
+        if isvector(mus) || isscalar(mus)
+            if isLessThanR2021b % on CPU
+                matrix(:,:,iMtx) = bsxfun(@times,mus(:),matrix(:,:,iMtx));
+            else % on CPU
+                matrix(:,:,iMtx) = mus(:).*matrix(:,:,iMtx);
+            end
+        else
+            if isLessThanR2021b % on CPU
+                matrix(:,:,iMtx) = bsxfun(@times,mus(:,iMtx),matrix(:,:,iMtx));
+            else % on CPU
+                matrix(:,:,iMtx) = mus(:,iMtx).*matrix(:,:,iMtx);
+            end
         end
     end
 end
 end
 
-function [vt,vb] = rot_(vt,vb,angle,useGpu,isLessThanR2021b)
+function [vt,vb] = rot_(vt,vb,angle,isLessThanR2021b)
 c = cos(angle);
 s = sin(angle);
-if useGpu
-    u  = arrayfun(@(s,vt,vb) s.*(vt+vb),s,vt,vb);
-    vt = arrayfun(@(c,s,vt,u) (c+s).*vt-u,c,s,vt,u);
-    vb = arrayfun(@(c,s,vb,u) (c-s).*vb+u,c,s,vb,u);
-elseif isLessThanR2021b
+if isLessThanR2021b
     u  = bsxfun(@times,s,bsxfun(@plus,vt,vb));
     vt = bsxfun(@minus,bsxfun(@times,c+s,vt),u);
-    vb = bsxfun(@plus,bsxfun(@times,c-s,vb),u);    
+    vb = bsxfun(@plus,bsxfun(@times,c-s,vb),u);
 else
     u  = s.*(vt+vb);
     vt = (c+s).*vt-u;
