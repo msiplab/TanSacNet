dc4f3e4b4bad815622d6b082af4cd5576c285e25
Eisuke_Kobayashi <Eisuke_Kobayashi@DESKTOP-HN1C2OD>
2022-09-16 14:14:44 +0900

    add lsunChannelConcatenation3dLayer and TestCase

---
+++ +tansacnet/+lsun/lsunChannelConcatenation3dLayer.m
@@ -0,0 +1,83 @@
+classdef lsunChannelConcatenation3dLayer < nnet.layer.Layer %#codegen
+    %NSOLTCHANNELCONCATENATION2DLAYER
+    %
+    %   ２コンポーネント入力(nComponents=2のみサポート):
+    %      nRows x nCols x nLays x (nChsTotal-1) x nSamples
+    %      nRows x nCols x nLays x 1 x nSamples
+    %
+    %   １コンポーネント出力(nComponents=1のみサポート):
+    %      nChsTotal x nRows x nCols x nLays x nSamples
+    %
+
+    
+    properties
+        % (Optional) Layer properties.
+        
+        % Layer properties go here.
+    end
+    
+    methods
+        function layer = lsunChannelConcatenation3dLayer(varargin)
+            % (Optional) Create a myLayer.
+            % This function must have the same name as the class.
+            p = inputParser;
+            addParameter(p,'Name','')
+            parse(p,varargin{:})
+            
+            % Layer constructor function goes here.
+            layer.Name = p.Results.Name;
+            layer.Description =  "Channel concatenation";
+            layer.Type = '';
+            %layer.NumInputs = 2;
+            layer.InputNames = { 'ac', 'dc' };
+            
+        end
+        
+        function Z = predict(~, Xac,Xdc)
+            % Forward input data through the layer at prediction time and
+            % output the result.
+            %
+            % Inputs:
+            %         layer       - Layer to forward propagate through
+            %         X1, X2      - Input data (2 components)
+            % Outputs:
+            %         Z           - Outputs of layer forward function
+            %  
+            
+            if isdlarray(Xac) 
+                Xac = stripdims(Xac);
+            end                        
+            
+            if isdlarray(Xdc) 
+                Xdc = stripdims(Xdc);
+            end                                    
+            
+            % Layer forward function for prediction goes here.
+            Z = permute(cat(4,Xdc,Xac),[4 1 2 3 5]);
+        end
+        
+        function [dLdXac,dLdXdc] = backward(~,~, ~, ~, dLdZ, ~)
+            % (Optional) Backward propagate the derivative of the loss  
+            % function through the layer.
+            %
+            % Inputs:
+            %         layer             - Layer to backward propagate through
+            %         X1, ..., Xn       - Input data
+            %         Z1, ..., Zm       - Outputs of layer forward function            
+            %         dLdZ1, ..., dLdZm - Gradients propagated from the next layers
+            %         memory            - Memory value from forward function
+            % Outputs:
+            %         dLdX1, ..., dLdXn - Derivatives of the loss with respect to the
+            %                             inputs
+            %         dLdW1, ..., dLdWk - Derivatives of the loss with respect to each
+            %                             learnable parameter
+            
+            % Layer forward function for prediction goes here.
+            %dLdX1 = dLdZ(:,:,1,:);
+            %dLdX2 = dLdZ(:,:,2:end,:);
+            dLdXac = ipermute(dLdZ(2:end,:,:,:,:),[4 1 2 3 5]);            
+            dLdXdc = ipermute(dLdZ(1,:,:,:,:),[4 1 2 3 5]);
+        end
+    end
+    
+end
---
+++ +tansacnet/+testcase/+lsun/lsunChannelConcatenation3dLayerTestCase.m
@@ -0,0 +1,124 @@
+classdef lsunChannelConcatenation3dLayerTestCase < matlab.unittest.TestCase
+    %NSOLTCHANNELCONCATENATION2DLAYERTESTCASE
+    %
+    %   ２コンポーネント入力(nComponents=2のみサポート):
+    %      nRows x nCols x (nChsTotal-1) x nSamples    
+    %      nRows x nCols x 1 x nSamples
+    %
+    %   １コンポーネント出力(nComponents=1のみサポート):
+    %       nChsTotal x nRows x nCols xnSamples
+    %
+    
+    properties (TestParameter)
+        nchs = { [3 3], [4 4], [32 32] };
+        datatype = { 'single', 'double' };
+        nrows = struct('small', 1,'medium', 4, 'large', 16);
+        ncols = struct('small', 1,'medium', 4, 'large', 16);
+        nlays = struct('small', 1,'medium', 4, 'large', 16);
+        batch = { 1, 8 };
+    end
+    
+    methods (TestClassTeardown)
+        function finalCheck(~)
+            import tansacnet.lsun.*
+            layer = lsunChannelConcatenation3dLayer();
+            fprintf("\n --- Check layer for 3-D images ---\n");
+            checkLayer(layer,{[8 8 8 5], [8 8 8 1]},...
+                'ObservationDimension',5,...
+                'CheckCodegenCompatibility',false)
+        end
+    end
+    
+    methods (Test)
+        
+        function testConstructor(testCase)
+            
+            % Expected values
+            expctdName = 'Cn';
+            expctdDescription = "Channel concatenation";
+            
+            % Instantiation of target class
+            import tansacnet.lsun.*
+            layer = lsunChannelConcatenation2dLayer('Name',expctdName);
+            
+            % Actual values
+            actualName = layer.Name;
+            actualDescription = layer.Description;
+            
+            % Evaluation
+            testCase.verifyEqual(actualName,expctdName);    
+            testCase.verifyEqual(actualDescription,expctdDescription);
+        end
+        
+        function testPredict(testCase,nchs,nrows,ncols,nlays,batch,datatype)
+            
+            import matlab.unittest.constraints.IsEqualTo
+            import matlab.unittest.constraints.AbsoluteTolerance
+            tolObj = AbsoluteTolerance(1e-6,single(1e-6));
+            
+            % Parameters
+            nSamples = batch;
+            nChsTotal = sum(nchs);
+            % nRows x nCols x nLays x (nChsTotal-1) x nSamples 
+            Xac = randn(nrows,ncols,nlays,nChsTotal-1,nSamples,datatype);
+            % nRows x nCols x nLays x 1 x nSamples
+            Xdc = randn(nrows,ncols,nlays,1,nSamples,datatype);
+
+            
+            % Expected values
+            % nChsTotal x nRows x nCols x nSamples
+            expctdZ = permute(cat(4,Xdc,Xac),[4 1 2 3 5]);
+            
+            % Instantiation of target class
+            import tansacnet.lsun.*
+            layer = lsunChannelConcatenation3dLayer('Name','Cn');
+            
+            % Actual values
+            actualZ = layer.predict(Xac,Xdc);
+            
+            % Evaluation
+            testCase.verifyInstanceOf(actualZ,datatype);
+            testCase.verifyThat(actualZ,...
+                IsEqualTo(expctdZ,'Within',tolObj));
+            
+        end
+                
+        function testBackward(testCase,nchs,nrows,ncols,nlays,batch,datatype)
+            
+            import matlab.unittest.constraints.IsEqualTo
+            import matlab.unittest.constraints.AbsoluteTolerance
+            tolObj = AbsoluteTolerance(1e-6,single(1e-6));
+            
+            % Parameters
+            nSamples = batch;
+            nChsTotal = sum(nchs);
+            % nChsTotal x nRows x nCols x nLays x nSamples
+            %dLdZ = randn(nrows,ncols,nlays,nChsTotal,nSamples,datatype);
+            dLdZ = randn(nChsTotal,nrows,ncols,nlays,nSamples,datatype);
+            
+            % Expected values
+            % nRows x nCols x nLays x (nChsTotal-1) x nSamples
+            expctddLdXac = permute(dLdZ(2:end,:,:,:,:),[2 3 4 1 5]);
+            % nRows x nCols x nLays x 1 x nSamples
+            expctddLdXdc = permute(dLdZ(1,:,:,:,:),[2 3 4 1 5]);
+            
+            % Instantiation of target class
+            import tansacnet.lsun.*
+            layer = lsunChannelConcatenation3dLayer('Name','Cn');
+            
+            % Actual values
+            [actualdLdXac,actualdLdXdc] = layer.backward([],[],[],dLdZ,[]);
+            
+            % Evaluation
+            testCase.verifyInstanceOf(actualdLdXdc,datatype);
+            testCase.verifyInstanceOf(actualdLdXac,datatype);            
+            testCase.verifyThat(actualdLdXdc,...
+                IsEqualTo(expctddLdXdc,'Within',tolObj));
+            testCase.verifyThat(actualdLdXac,...
+                IsEqualTo(expctddLdXac,'Within',tolObj));            
+            
+        end
+        
+    end
+    
+end
