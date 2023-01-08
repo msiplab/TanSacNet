263d22ff46dcde365ea2e501c6fc571940eadd46
shodimaggio <shogo@eng.niigata-u.ac.jp>
2023-01-08 16:34:38 +0900

    Added lsunBlockDct1dLayer

---
+++ code/+tansacnet/+lsun/lsunBlockDct1dLayer.m
@@ -0,0 +1,151 @@
+classdef lsunBlockDct1dLayer < nnet.layer.Layer %#codegen
+    %NSOLTBLOCKDCT1DLAYER
+    %
+    %   ベクトル配列をブロック配列を入力:
+    %      nComponents x nSamples x (Stride(1)xnCols) 
+    %
+    %   コンポーネント別に出力(nComponents):
+    %      nDecs x nCols x nSamples
+    %
+    % Requirements: MATLAB R2022b
+    %
+    % Copyright (c) 2023, Shogo MURAMATSU
+    %
+    % All rights reserved.
+    %
+    % Contact address: Shogo MURAMATSU,
+    %                Faculty of Engineering, Niigata University,
+    %                8050 2-no-cho Ikarashi, Nishi-ku,
+    %                Niigata, 950-2181, JAPAN
+    %
+    % http://msiplab.eng.niigata-u.ac.jp/
+    
+    properties
+        % (Optional) Layer properties.
+        Stride
+        
+        % Layer properties go here.
+    end
+    
+    properties (Access = private)
+        C
+    end
+    
+    methods
+        function layer = lsunBlockDct1dLayer(varargin)
+            % (Optional) Create a myLayer.
+            % This function must have the same name as the class.
+            %import tansacnet.utility.Direction
+            p = inputParser;
+            addParameter(p,'Stride',[])
+            addParameter(p,'Name','')
+            addParameter(p,'NumberOfComponents',1);
+            parse(p,varargin{:})
+            
+            % Layer constructor function goes here.
+            layer.Stride = p.Results.Stride;
+            layer.Name = p.Results.Name;
+            layer.Description = "Block DCT of size " ...
+                + layer.Stride;
+            layer.Type = '';
+            layer.NumOutputs = p.Results.NumberOfComponents;
+            layer.NumInputs = 1;
+            
+            dec = layer.Stride;
+            %
+            C_ = dctmtx(dec);
+            C_ = [ C_(1:2:end,:) ; C_(2:2:end,:) ];
+            %
+            Ce = C_(1:ceil(dec/2),:);
+            Co = C_(ceil(dec/2)+1:end,:);
+            layer.C = [Ce; Co];
+            
+        end
+        
+        function varargout = predict(layer, X)
+            % Forward input data through the layer at prediction time and
+            % output the result.
+            %
+            % Inputs:
+            %         layer       - Layer to forward propagate through
+            %         X           - Input data
+            % Outputs:
+            %         Z1, ..., Zm - Outputs of layer forward function
+            %import tansacnet.utility.Direction
+
+            nComponents = layer.NumOutputs;
+            varargout = cell(1,nComponents);
+            
+            if isdlarray(X)
+                X = stripdims(X);
+            end
+            
+            % Layer forward function for prediction goes here.
+            decFactor = layer.Stride;
+            %
+            C_ = layer.C;
+            %
+            %disp('size(X)')
+            %size(X)
+            nSamples = size(X,2);            
+            seqlen = size(X,3);
+            nCols = seqlen/decFactor;
+            %
+            for iComponent = 1:nComponents
+               
+                arrayX = permute(reshape(X(iComponent,:,:),...
+                    nSamples,decFactor,nCols),[2 3 1]);
+
+                if isgpuarray(X)
+                    varargout{iComponent} = pagefun(@mtimes,C_,arrayX);
+                else
+                    varargout{iComponent} = reshape(...
+                        C_*reshape(arrayX,decFactor,[]),...
+                       decFactor,nCols,nSamples);
+                end
+            end
+
+        end
+        
+        function dLdX = backward(layer, varargin)
+            % (Optional) Backward propagate the derivative of the loss
+            % function through the layer.
+            %
+            % Inputs:
+            %         layer             - Layer to backward propagate through
+            %         X1, ..., Xn       - Input data
+            %         Z1, ..., Zm       - Outputs of layer forward function
+            %         dLdZ1, ..., dLdZm - Gradients propagated from the next layers
+            %         memory            - Memory value from forward function
+            % Outputs:
+            %         dLdX1, ..., dLdXn - Derivatives of the loss with respect to the
+            %                             inputs
+            %         dLdW1, ..., dLdWk - Derivatives of the loss with respect to each
+            %                             learnable parameter
+            %import tansacnet.utility.Direction
+            
+            nComponents = layer.NumOutputs;
+            decFactor = layer.Stride;
+            C_T = layer.C.';
+            %
+            dLdZ = varargin{layer.NumInputs+layer.NumOutputs+1};
+            nCols = size(dLdZ,2);
+            seqlen = decFactor*nCols;
+            nSamples = size(dLdZ,3);
+            dLdX = zeros(nComponents,nSamples,seqlen,'like',dLdZ);
+            %
+            for iComponent = 1:nComponents
+                dLdZ = varargin{layer.NumInputs+layer.NumOutputs+iComponent};
+                if isgpuarray(dLdZ)
+                    arrayX = pagefun(@mtimes,C_T,dLdZ);
+                else
+                    arrayX = C_T*reshape(dLdZ,decFactor,[]);
+                end
+                dLdX(iComponent,:,:) = ...
+                    permute(reshape(arrayX,seqlen,nSamples),[2 3 1]);
+            end
+        end
+    end
+    
+end
+
---
+++ code/+tansacnet/+testcase/+lsun/lsunBlockDct1dLayerTestCase.m
@@ -0,0 +1,223 @@
+classdef lsunBlockDct1dLayerTestCase < matlab.unittest.TestCase
+    %NSOLTBLOCKDCT1DLAYERTESTCASE
+    %
+    %   ベクトル配列をブロック配列を入力:
+    %      nComponents x nSamples x (Stride(1)xnCols) 
+    %
+    %   コンポーネント別に出力:
+    %      nDecs x nCols x nSamples
+    %
+    % Requirements: MATLAB R2022b
+    %
+    % Copyright (c) 2023, Shogo MURAMATSU
+    %
+    % All rights reserved.
+    %
+    % Contact address: Shogo MURAMATSU,
+    %                Faculty of Engineering, Niigata University,
+    %                8050 2-no-cho Ikarashi, Nishi-ku,
+    %                Niigata, 950-2181, JAPAN
+    %
+    % http://msiplab.eng.niigata-u.ac.jp/
+    
+    properties (TestParameter)
+        stride = { 1, 2, 4, 8 };
+        datatype = { 'single', 'double' };
+        seqlen = struct('small', 8,'medium', 16, 'large', 32);
+    end
+    
+    methods (TestClassTeardown)
+        
+        function finalCheck(~)
+            import tansacnet.lsun.*
+            fprintf("\n --- Check layer for 1-D vector sequenses ---\n");
+            layer = lsunBlockDct1dLayer('Stride',2);
+            checkLayer(layer,[1 8 8],'ObservationDimension',2,...
+                'CheckCodegenCompatibility',false)
+        end
+        
+    end
+    
+    methods (Test)
+        
+        function testConstructor(testCase, stride)
+            
+            % Expected values
+            expctdName = 'E0';
+            expctdDescription = "Block DCT of size " ...
+                + stride(1);
+            
+            % Instantiation of target class
+            import tansacnet.lsun.*
+            layer = lsunBlockDct1dLayer(...
+                'Stride',stride,...
+                'Name',expctdName);
+            
+            % Actual values
+            actualName = layer.Name;
+            actualDescription = layer.Description;
+            
+            % Evaluation
+            testCase.verifyEqual(actualName,expctdName);
+            testCase.verifyEqual(actualDescription,expctdDescription);
+
+        end
+
+        function testPredict(testCase, ...
+                stride, seqlen, datatype)
+            import matlab.unittest.constraints.IsEqualTo
+            import matlab.unittest.constraints.AbsoluteTolerance
+            tolObj = AbsoluteTolerance(1e-5,single(1e-5));
+            
+            % Parameters
+            nSamples = 8;
+            nComponents = 1;
+            X = rand(nComponents, nSamples, seqlen, datatype);
+            
+            % Expected values
+            ncols = ceil(seqlen/stride);
+            expctdZ = zeros(stride,ncols,nSamples,datatype);
+            for iSample = 1:nSamples
+                % Block DCT
+                U = reshape(X(nComponents,iSample,:),stride,[]);
+                if stride > 1
+                    Y = dct(U);
+                    % Rearrange the DCT Coefs.
+                    A = testCase.permuteDctCoefs_(Y);
+                    expctdZ(:,:,iSample) = ...
+                        reshape(A,stride,ncols);
+                else
+                    expctdZ(:,:,iSample) = ...
+                        reshape(U,stride,ncols);
+                end
+            end
+            
+            % Instantiation of target class
+            import tansacnet.lsun.*
+            layer = lsunBlockDct1dLayer(...
+                'Stride',stride,...
+                'Name','E0');
+            
+            % Actual values
+            actualZ = layer.predict(X);
+            
+            % Evaluation
+            testCase.verifyInstanceOf(actualZ,datatype);
+            testCase.verifyThat(actualZ,...
+                IsEqualTo(expctdZ,'Within',tolObj));
+            
+        end
+                
+        function testForward(testCase, ...
+                stride, seqlen, datatype)
+            import matlab.unittest.constraints.IsEqualTo
+            import matlab.unittest.constraints.AbsoluteTolerance
+            tolObj = AbsoluteTolerance(1e-5,single(1e-5));
+            
+            % Parameters
+            nSamples = 8;
+            nComponents = 1;
+            X = rand(nComponents, nSamples, seqlen, datatype);
+            
+            % Expected values
+            ncols = ceil(seqlen/stride);
+            expctdZ = zeros(stride,ncols,nSamples,datatype);
+            for iSample = 1:nSamples
+                % Block DCT
+                U = reshape(X(nComponents,iSample,:),stride,[]);
+                if stride > 1
+                    Y = dct(U);
+                    % Rearrange the DCT Coefs.
+                    A = testCase.permuteDctCoefs_(Y);
+                    expctdZ(:,:,iSample) = ...
+                        reshape(A,stride,ncols);
+                else
+                    expctdZ(:,:,iSample) = ...
+                        reshape(U,stride,ncols);
+                end
+            end
+            
+            % Instantiation of target class
+            import tansacnet.lsun.*
+            layer = lsunBlockDct1dLayer(...
+                'Stride',stride,...
+                'Name','E0');
+            
+            % Actual values
+            actualZ = layer.forward(X);
+            
+            % Evaluation
+            testCase.verifyInstanceOf(actualZ,datatype);
+            testCase.verifyThat(actualZ,...
+                IsEqualTo(expctdZ,'Within',tolObj));
+            
+        end
+        
+        function testBackward(testCase, ...
+                stride, seqlen, datatype)
+            import matlab.unittest.constraints.IsEqualTo
+            import matlab.unittest.constraints.AbsoluteTolerance
+            tolObj = AbsoluteTolerance(1e-5,single(1e-5));
+            
+            % Parameters
+            nSamples = 8;
+            ncols = seqlen/stride;
+            nComponents = 1;
+            %dLdZ = rand(nrows,ncols,nDecs,nSamples,datatype);
+            dLdZ = rand(stride,ncols,nSamples,datatype);
+            
+            % Expected values
+            expctddLdX = zeros(nComponents,nSamples,seqlen,datatype);
+            for iSample = 1:nSamples
+                A = reshape(dLdZ(:,:,iSample),stride,[]);
+                if stride > 1
+                    Y = testCase.permuteIdctCoefs_(A,stride);
+                    expctddLdX(nComponents,iSample,:) = ...
+                        reshape(idct(Y),nComponents,1,seqlen,[]);
+                else
+                    expctddLdX(nComponents,iSample,:) = ...
+                        reshape(A,nComponents,1,seqlen,[]);
+                end
+            end
+
+            % Instantiation of target class
+            import tansacnet.lsun.*
+            layer = lsunBlockDct1dLayer(...
+                'Stride',stride,...
+                'Name','E0');
+            
+            % Actual values
+            actualdLdX = layer.backward([],[],dLdZ,[]);
+            
+            % Evaluation
+            testCase.verifyInstanceOf(actualdLdX,datatype);
+            testCase.verifyThat(actualdLdX,...
+                IsEqualTo(expctddLdX,'Within',tolObj));
+            
+        end
+
+    end
+        
+    methods (Static, Access = private)
+        
+        function value = permuteDctCoefs_(x)
+            coefs = x;
+            ce = coefs(1:2:end,:);
+            co = coefs(2:2:end,:);
+            value = [ ce ; co ];
+        end
+        function value = permuteIdctCoefs_(x,stride)
+            coefs = x;
+            nHDecse = ceil(stride/2);
+            nHDecso = floor(stride/2);
+            ce = coefs(         1:  nHDecse,:);
+            co = coefs(nHDecse+1:nHDecse+nHDecso,:);
+            value = zeros(stride,numel(coefs)/stride,'like',x);
+            value(1:2:stride,:) = ce;
+            value(2:2:stride,:) = co;
+        end
+
+    end
+        
+end
+
