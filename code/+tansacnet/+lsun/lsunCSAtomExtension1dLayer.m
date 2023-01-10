12121ceaeacdbce1759ca44dfef77bbd90d5ce66
shodimaggio <shogo@eng.niigata-u.ac.jp>
2023-01-10 17:07:01 +0900

    Added lsunCSAtomExtension1dLayer

---
+++ code/+tansacnet/+lsun/lsunCSAtomExtension1dLayer.m
@@ -0,0 +1,159 @@
+classdef lsunCSAtomExtension1dLayer < nnet.layer.Layer %#codegen
+    %LSUNCSATOMEXTENSION1DLAYER
+    %
+    %   コンポーネント別に入力(nComponents=1のみサポート):
+    %      nChsTotal x nSamples x nBlks
+    %
+    %   コンポーネント別に出力(nComponents=1のみサポート):
+    %      nChsTotal x nSamples x nBlks
+    %
+    % Requirements: MATLAB R2022b
+    %
+    % Copyright (c) 2023, Shogo MURAMATSU
+    %
+    % All rights reserved.
+    %
+    % Contact address: Shogo MURAMATSU,
+    %                Faculty of Engineering, Niigata University,
+    %                8050 2-no-cho Ikarashi, Nishi-ku,
+    %                Niigata, 950-2181, JAPAN
+    %
+    % http://msiplab.eng.niigata-u.ac.jp/
+    
+    properties
+        % (Optional) Layer properties.
+        Stride
+        Direction
+        TargetChannels
+        
+        % Layer properties go here.
+    end
+    
+    methods
+        function layer = lsunCSAtomExtension1dLayer(varargin)
+            % (Optional) Create a myLayer.
+            % This function must have the same name as the class.
+            p = inputParser;
+            addParameter(p,'Name','')
+            addParameter(p,'Stride',[])
+            addParameter(p,'Direction','')
+            addParameter(p,'TargetChannels','')
+            parse(p,varargin{:})
+            
+            % Layer constructor function goes here.
+            layer.Stride = p.Results.Stride;
+            layer.Name = p.Results.Name;
+            layer.Direction = p.Results.Direction;
+            layer.TargetChannels = p.Results.TargetChannels;
+            nChsTotal = prod(layer.Stride);
+            layer.Description =  layer.Direction ...
+                + " shift the " ...
+                + lower(layer.TargetChannels) ...
+                + "-channel Coefs. " ...
+                + "(ps,pa) = (" ...
+                + ceil(nChsTotal/2) + "," ...
+                + floor(nChsTotal/2) + ")";
+            
+            layer.Type = '';
+            
+        end
+        
+        function Z = predict(layer, X)
+            % Forward input data through the layer at prediction time and
+            % output the result.
+            %
+            % Inputs:
+            %         layer       - Layer to forward propagate through
+            %         X1, ..., Xn - Input data (n: # of components)
+            % Outputs:
+            %         Z           - Outputs of layer forward function
+            %
+            
+            % Layer forward function for prediction goes here.
+            dir = layer.Direction;
+            %
+            if strcmp(dir,'Right')
+                shift = [ 0 0 1 0 ];
+            elseif strcmp(dir,'Left')
+                shift = [ 0 0 -1 0 ];
+            elseif strcmp(dir,'Down')
+                shift = [ 0 1 0 0 ];
+            elseif strcmp(dir,'Up')
+                shift = [ 0 -1 0 0 ];
+            else
+                throw(MException('NsoltLayer:InvalidDirection',...
+                    '%s : Direction should be either of Right, Left, Down or Up',...
+                    layer.Direction))
+            end
+            %
+            Z = layer.atomext_(X,shift);
+        end
+        
+        function dLdX = backward(layer, ~, ~, dLdZ, ~)
+            % (Optional) Backward propagate the derivative of the loss  
+            % function through the layer.
+            %
+            % Inputs:
+            %         layer             - Layer to backward propagate through
+            %         X1, ..., Xn       - Input data
+            %         Z1, ..., Zm       - Outputs of layer forward function            
+            %         dLdZ1, ..., dLdZm - Gradients propagated from the next layers
+            %         memory            - Memory value from forward function
+            % Outputs:
+            %         dLdX1, ..., dLdXn - Derivatives of the loss with respect to the
+            %                             inputs
+            %         dLdW1, ..., dLdWk - Derivatives of the loss with respect to each
+            %                             learnable parameter
+            
+            % Layer forward function for prediction goes here.
+            dir = layer.Direction;
+
+            %
+            if strcmp(dir,'Right')
+                shift = [ 0 0 -1 0 ]; % Reverse
+            elseif strcmp(dir,'Left')
+                shift = [ 0 0 1 0 ];  % Reverse
+            elseif strcmp(dir,'Down')
+                shift = [ 0 -1 0 0 ];  % Reverse
+            elseif strcmp(dir,'Up')
+                shift = [ 0 1 0 0 ];  % Reverse
+            else
+                throw(MException('NsoltLayer:InvalidDirection',...
+                    '%s : Direction should be either of Right, Left, Down or Up',...
+                    layer.Direction))
+            end
+            %
+            dLdX = layer.atomext_(dLdZ,shift);
+        end
+        
+        function Z = atomext_(layer,X,shift)
+            nChsTotal = prod(layer.Stride);
+            ps = ceil(nChsTotal/2);
+            pa = floor(nChsTotal/2);
+            target = layer.TargetChannels;            
+            %
+            % Block butterfly
+            Xs = X(1:ps,:,:,:);
+            Xa = X(ps+1:ps+pa,:,:,:);
+            Ys =  bsxfun(@plus,Xs,Xa);
+            Ya =  bsxfun(@minus,Xs,Xa);
+            % Block circular shift
+            if strcmp(target,'Difference')
+                Ya = circshift(Ya,shift);
+            elseif strcmp(target,'Sum')
+                Ys = circshift(Ys,shift);
+            else
+                throw(MException('NsoltLayer:InvalidTargetChannels',...
+                    '%s : TaregetChannels should be either of Sum or Difference',...
+                    layer.TargetChannels))
+            end
+            % Block butterfly
+            Y =  cat(1,bsxfun(@plus,Ys,Ya),bsxfun(@minus,Ys,Ya));
+            % Output
+            Z = 0.5*Y; %ipermute(Y,[3 1 2 4])/2.0;
+        end
+        
+    end
+
+end
+
---
+++ code/+tansacnet/+testcase/+lsun/lsunCSAtomExtension1dLayerTestCase.m
@@ -0,0 +1,334 @@
+classdef lsunCSAtomExtension1dLayerTestCase < matlab.unittest.TestCase
+    %LSUNCSATOMEXTENSION1DLAYERTESTCASE
+    %
+    %   コンポーネント別に入力(nComponents=1のみサポート):
+    %      nChsTotal x nSamples x nBlks
+    %
+    %   コンポーネント別に出力(nComponents=1のみサポート):
+    %      nChsTotal x nSamples x nBlks
+    %
+    % Requirements: MATLAB R2022b
+    %
+    % Copyright (c) 2023, Shogo MURAMATSU
+    %
+    % All rights reserved.
+    %
+    % Contact address: Shogo MURAMATSU,
+    %                Faculty of Engineering, Niigata University,
+    %                8050 2-no-cho Ikarashi, Nishi-ku,
+    %                Niigata, 950-2181, JAPAN
+    %
+    % http://msiplab.eng.niigata-u.ac.jp/
+    
+    properties (TestParameter)
+        stride = { 2, 4, 8 };
+        datatype = { 'single', 'double' };
+        nblks = struct('small', 4,'medium', 8, 'large', 16);
+        dir = { 'Right', 'Left' };
+        target = { 'Sum', 'Difference' }
+    end
+    
+    methods (TestClassTeardown)
+        %{
+        function finalCheck(~)
+            import tansacnet.lsun.*
+            layer = lsunCSAtomExtension1dLayer(...
+                'Stride',2,...
+                'Direction','Right',...
+                'TargetChannels','Difference');
+            fprintf("\n --- Check layer for 1-D sequences ---\n");
+            checkLayer(layer,[2 8 8],...
+                'ObservationDimension',2,...
+                'CheckCodegenCompatibility',true)
+        end
+        %}
+    end
+    
+    methods (Test)
+        
+        function testConstructor(testCase, stride, target)
+            
+            % Parameters
+            nChsTotal = stride;
+
+            % Expected values
+            expctdName = 'Qn';
+            expctdDirection = 'Right';
+            expctdTargetChannels = target;
+            expctdDescription = "Right shift the " ...
+                + lower(target) ...
+                + "-channel Coefs. " ...
+                + "(ps,pa) = (" ...
+                + ceil(nChsTotal/2) + "," + floor(nChsTotal/2) + ")";
+            
+            % Instantiation of target class
+            import tansacnet.lsun.*
+            layer = lsunCSAtomExtension1dLayer(...
+                'Stride',stride,...
+                'Name',expctdName,...
+                'Direction',expctdDirection,...
+                'TargetChannels',expctdTargetChannels);
+            
+            % Actual values
+            actualName = layer.Name;
+            actualDirection = layer.Direction;
+            actualTargetChannels = layer.TargetChannels;
+            actualDescription = layer.Description;
+            
+            % Evaluation
+            testCase.verifyEqual(actualName,expctdName);
+            testCase.verifyEqual(actualDirection,expctdDirection);
+            testCase.verifyEqual(actualTargetChannels,expctdTargetChannels);
+            testCase.verifyEqual(actualDescription,expctdDescription);
+        end
+        
+        %{
+        function testPredictGrayscaleShiftDifferenceCoefs(testCase, ...
+                stride, nrows, ncols, dir, datatype)
+            
+            import matlab.unittest.constraints.IsEqualTo
+            import matlab.unittest.constraints.AbsoluteTolerance
+            tolObj = AbsoluteTolerance(1e-6,single(1e-6));
+            
+            % Parameters
+            nSamples = 8;
+            nChsTotal = prod(stride);
+            target_ = 'Difference';
+            % nChsTotal x nRows x nCols x nSamples
+            %X = randn(nrows,ncols,nChsTotal,nSamples,datatype);
+            X = randn(nChsTotal,nrows,ncols,nSamples,datatype);
+            
+            % Expected values
+            if strcmp(dir,'Right')
+                shift = [ 0 0  1 0 ];
+            elseif strcmp(dir,'Left')
+                shift = [ 0 0 -1 0 ];
+            elseif strcmp(dir,'Down')
+                shift = [ 0  1 0 0 ];
+            elseif strcmp(dir,'Up')
+                shift = [ 0 -1 0 0 ];
+            else
+                shift = [ 0 0 0 0 ];
+            end
+            % nRows x nCols x nChsTotal x nSamples
+            ps = ceil(nChsTotal/2);
+            pa = floor(nChsTotal/2);
+            % Block butterfly
+            Ys = X(1:ps,:,:,:);
+            Ya = X(ps+1:ps+pa,:,:,:);
+            Y =  [ Ys+Ya ; Ys-Ya ]/sqrt(2);
+            % Block circular shift
+            Y(ps+1:ps+pa,:,:,:) = circshift(Y(ps+1:ps+pa,:,:,:),shift);
+            % Block butterfly
+            Ys = Y(1:ps,:,:,:);
+            Ya = Y(ps+1:ps+pa,:,:,:);
+            Y =  [ Ys+Ya ; Ys-Ya ]/sqrt(2);
+            % Output
+            expctdZ = Y; %ipermute(Y,[3 1 2 4]);
+            
+            % Instantiation of target class
+            import tansacnet.lsun.*
+            layer = lsunCSAtomExtension1dLayer(...
+                'Stride',stride,...
+                'Name','Qn~',...
+                'Direction',dir,...
+                'TargetChannels',target_);
+            
+            % Actual values
+            actualZ = layer.predict(X);
+            
+            % Evaluation
+            testCase.verifyInstanceOf(actualZ,datatype);
+            testCase.verifyThat(actualZ,...
+                IsEqualTo(expctdZ,'Within',tolObj));
+            
+        end
+        
+        function testPredictGrayscaleShiftSumCoefs(testCase, ...
+                stride, nrows, ncols, dir, datatype)
+            
+            import matlab.unittest.constraints.IsEqualTo
+            import matlab.unittest.constraints.AbsoluteTolerance
+            tolObj = AbsoluteTolerance(1e-6,single(1e-6));
+            
+            % Parameters
+            nSamples = 8;
+            nChsTotal = prod(stride);
+            target_ = 'Sum';
+            % nChsTotal x nRows x nCols x nSamples
+            %X = randn(nrows,ncols,nChsTotal,nSamples,datatype);
+            X = randn(nChsTotal,nrows,ncols,nSamples,datatype);
+            
+            % Expected values
+            if strcmp(dir,'Right')
+                shift = [ 0 0  1 0 ];
+            elseif strcmp(dir,'Left')
+                shift = [ 0 0 -1 0 ];
+            elseif strcmp(dir,'Down')
+                shift = [ 0  1 0 0 ];
+            elseif strcmp(dir,'Up')
+                shift = [ 0 -1 0 0 ];
+            else
+                shift = [ 0 0 0 0 ];
+            end
+            % nChsTotal x nRows x nCols x nSamples
+            ps = ceil(nChsTotal/2);
+            pa = floor(nChsTotal/2);
+            % Block butterfly
+            Ys = X(1:ps,:,:,:);
+            Ya = X(ps+1:ps+pa,:,:,:);
+            Y =  [ Ys+Ya ; Ys-Ya ]/sqrt(2);
+            % Block circular shift
+            Y(1:ps,:,:,:) = circshift(Y(1:ps,:,:,:),shift);
+            % Block butterfly
+            Ys = Y(1:ps,:,:,:);
+            Ya = Y(ps+1:ps+pa,:,:,:);
+            Y =  [ Ys+Ya ; Ys-Ya ]/sqrt(2);
+            % Output
+            expctdZ = Y; %ipermute(Y,[3 1 2 4]);
+            
+            % Instantiation of target class
+            import tansacnet.lsun.*
+            layer = lsunCSAtomExtension1dLayer(...
+                'Stride',stride,...
+                'Name','Qn~',...
+                'Direction',dir,...
+                'TargetChannels',target_);
+            
+            % Actual values
+            actualZ = layer.predict(X);
+            
+            % Evaluation
+            testCase.verifyInstanceOf(actualZ,datatype);
+            testCase.verifyThat(actualZ,...
+                IsEqualTo(expctdZ,'Within',tolObj));
+            
+        end
+        
+        function testBackwardGrayscaleShiftDifferenceCoefs(testCase, ...
+                stride, nrows, ncols, dir, datatype)
+            
+            import matlab.unittest.constraints.IsEqualTo
+            import matlab.unittest.constraints.AbsoluteTolerance
+            tolObj = AbsoluteTolerance(1e-6,single(1e-6));
+            
+            % Parameters
+            nSamples = 8;
+            nChsTotal = prod(stride);
+            target_ = 'Difference';
+            % nChsTotal x nRows x nCols x nSamples
+            %dLdZ = randn(nrows,ncols,nChsTotal,nSamples,datatype);
+            dLdZ = randn(nChsTotal,nrows,ncols,nSamples,datatype);
+            
+            % Expected values
+            if strcmp(dir,'Right')
+                shift = [ 0 0 -1 0 ]; % Reverse
+            elseif strcmp(dir,'Left')
+                shift = [ 0 0 1 0 ]; % Reverse
+            elseif strcmp(dir,'Down')
+                shift = [ 0 -1 0 0 ]; % Reverse
+            elseif strcmp(dir,'Up')
+                shift = [ 0 1 0 0 ]; % Reverse
+            else
+                shift = [ 0 0 0 0 ]; % Reverse
+            end
+            % nChsTotal x nRows x nCols x nSamples
+            ps = ceil(nChsTotal/2);
+            pa = floor(nChsTotal/2);
+            Y = dLdZ; %permute(dLdZ,[3 1 2 4]); % [ch ver hor smpl]
+            % Block butterfly
+            Ys = Y(1:ps,:,:,:);
+            Ya = Y(ps+1:ps+pa,:,:,:);
+            Y =  [ Ys+Ya ; Ys-Ya ]/sqrt(2);
+            % Block circular shift
+            Y(ps+1:ps+pa,:,:,:) = circshift(Y(ps+1:ps+pa,:,:,:),shift);
+            % Block butterfly
+            Ys = Y(1:ps,:,:,:);
+            Ya = Y(ps+1:ps+pa,:,:,:);
+            Y =  [ Ys+Ya ; Ys-Ya ]/sqrt(2);
+            % Output
+            expctddLdX = Y; %ipermute(Y,[3 1 2 4]);
+            
+            % Instantiation of target class
+            import tansacnet.lsun.*
+            layer = lsunCSAtomExtension1dLayer(...
+                'Stride',stride,...
+                'Name','Qn',...
+                'Direction',dir,...
+                'TargetChannels',target_);
+            
+            % Actual values
+            actualdLdX = layer.backward([],[],dLdZ,[]);
+            
+            % Evaluation
+            testCase.verifyInstanceOf(actualdLdX,datatype);
+            testCase.verifyThat(actualdLdX,...
+                IsEqualTo(expctddLdX,'Within',tolObj));
+            
+        end
+        
+        function testBackwardGrayscaleShiftSumCoefs(testCase, ...
+                stride, nrows, ncols, dir, datatype)
+            
+            import matlab.unittest.constraints.IsEqualTo
+            import matlab.unittest.constraints.AbsoluteTolerance
+            tolObj = AbsoluteTolerance(1e-6,single(1e-6));
+            
+            % Parameters
+            nSamples = 8;
+            nChsTotal = prod(stride);
+            target_ = 'Sum';
+            % nChsTotal x nRows x nCols x nSamples
+            %dLdZ = randn(nrows,ncols,nChsTotal,nSamples,datatype);
+            dLdZ = randn(nChsTotal,nrows,ncols,nSamples,datatype);
+            
+            % Expected values
+            if strcmp(dir,'Right')
+                shift = [ 0 0 -1 0 ]; % Reverse
+            elseif strcmp(dir,'Left')
+                shift = [ 0 0  1 0 ]; % Reverse
+            elseif strcmp(dir,'Down')
+                shift = [ 0 -1 0 0 ]; % Reverse
+            elseif strcmp(dir,'Up')
+                shift = [ 0 1 0 0 ]; % Reverse
+            else
+                shift = [ 0 0 0 0 ];
+            end
+            % nChsTotal x nRows x nCols x nSamples
+            ps = ceil(nChsTotal/2);
+            pa = floor(nChsTotal/2);
+            Y = dLdZ; %permute(dLdZ,[3 1 2 4]); % [ch ver hor smpl]
+            % Block butterfly
+            Ys = Y(1:ps,:,:,:);
+            Ya = Y(ps+1:ps+pa,:,:,:);
+            Y =  [ Ys+Ya ; Ys-Ya ]/sqrt(2);
+            % Block circular shift
+            Y(1:ps,:,:,:) = circshift(Y(1:ps,:,:,:),shift);
+            % Block butterfly
+            Ys = Y(1:ps,:,:,:);
+            Ya = Y(ps+1:ps+pa,:,:,:);
+            Y =  [ Ys+Ya ; Ys-Ya ]/sqrt(2);
+            % Output
+            expctddLdX = Y; %ipermute(Y,[3 1 2 4]);
+            
+            % Instantiation of target class
+            import tansacnet.lsun.*
+            layer = lsunCSAtomExtension1dLayer(...
+                'Stride',stride,...
+                'Name','Qn',...
+                'Direction',dir,...
+                'TargetChannels',target_);
+            
+            % Actual values
+            actualdLdX = layer.backward([],[],dLdZ,[]);
+            
+            % Evaluation
+            testCase.verifyInstanceOf(actualdLdX,datatype);
+            testCase.verifyThat(actualdLdX,...
+                IsEqualTo(expctddLdX,'Within',tolObj));
+            
+        end
+        %}
+    end
+    
+end
