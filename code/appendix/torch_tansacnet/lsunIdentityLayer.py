8c05bd225db207f2707434cf164855f5ef329985
shodimaggio <shogo@eng.niigata-u.ac.jp>
2024-06-15 07:19:28 +0900

    Added LsunIdentityLayer

---
+++ code/appendix/torch_tansacnet/lsunIdentityLayer.py
@@ -0,0 +1,29 @@
+import torch
+import torch.nn as nn
+
+class LsunIdentityLayer(nn.Module):
+    """
+    LSUNIDENTITYLAYER
+
+    Requirements: Python 3.10/11.x, PyTorch 2.3.x
+
+    Copyright (c) 2024, Shogo MURAMATSU
+
+    All rights reserved.
+
+    Contact address: Shogo MURAMATSU,
+                    Faculty of Engineering, Niigata University,
+                    8050 2-no-cho Ikarashi, Nishi-ku,
+                    Niigata, 950-2181, JAPAN
+
+    http://www.eng.eng.niigata-u.ac.jp/~msiplab/
+    """
+
+    def __init__(self,
+        name=''):
+        super(LsunIdentityLayer, self).__init__()
+        self.name = name
+        self.description = "Identity"        
+
+    def forward(self,X):
+        return X
