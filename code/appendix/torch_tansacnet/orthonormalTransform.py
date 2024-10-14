5b698ef34e0e727380e663421a6f6eea9e08c995
Shogo MURAMATSU <shogo@eng.niigata-u.ac.jp>
2024-10-14 10:21:30 +0900

    Refactored orthonormalTransform.py

--- code/appendix/torch_tansacnet/orthonormalTransform.py
+++ code/appendix/torch_tansacnet/orthonormalTransform.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.autograd as autograd
-import math
+#import math
 #import numpy as np
 from .lsunLayerExceptions import InvalidMode, InvalidMus, InvalidAngles
 
@@ -389,7 +389,7 @@ class SingleOrthonormalMatrixGenerationSystem:
         nAngles = len(angles)
 
         # Number of dimensions
-        nDims = int((1+math.sqrt(1+8*nAngles))/2)
+        nDims = int((1+torch.sqrt(1+8*nAngles))/2)
 
         # Setup of mus, which is send to the same device with angles
         if isinstance(mus, int) or isinstance(mus, float):
@@ -399,14 +399,46 @@ class SingleOrthonormalMatrixGenerationSystem:
         else:
             mus = mus.to(dtype=self.dtype,device=angles.device)
 
+        if self.partial_difference:
+            matrix = self.fcn_orthmtxgen_diff(angles, nDims, index_pd_angle)
+        else:
+            matrix = self.fcn_orthmtxgen(angles, nDims)
+
+        matrix = mus.view(-1,1) * matrix
+
+        return matrix 
+
+    def fcn_orthmtxgen_diff(self, angles, nDims, index_pd_angle):
+        matrix = torch.zeros(nDims,dtype=self.dtype,device=angles.device,requires_grad=False)
+        iAng = 0
+        for iTop in range(nDims-1):
+            vt = matrix[iTop,:]
+            for iBtm in range(iTop+1,nDims):
+                if iAng >= index_pd_angle:
+                    angle = angles[iAng]
+                    if iAng == index_pd_angle:
+                        angle = angle + torch.pi/2. #math.pi/2.
+                    c = torch.cos(angle)
+                    s = torch.sin(angle)
+                    vb = matrix[iBtm,:]
+                    #
+                    u  = s*(vt + vb)
+                    vt = (c + s)*vt
+                    vb = (c - s)*vb
+                    vt = vt - u
+                    matrix[iBtm,:] = vb + u
+                iAng = iAng + 1
+            matrix[iTop,:] = vt
+
+        return matrix
+    
+    def fcn_orthmtxgen(self,angles, nDims):
         matrix = torch.eye(nDims,dtype=self.dtype,device=angles.device,requires_grad=False)
         iAng = 0
         for iTop in range(nDims-1):
             vt = matrix[iTop,:]
             for iBtm in range(iTop+1,nDims):
                 angle = angles[iAng]
-                if self.partial_difference and iAng == index_pd_angle:
-                    angle = angle + math.pi/2.
                 c = torch.cos(angle)
                 s = torch.sin(angle)
                 vb = matrix[iBtm,:]
@@ -415,11 +447,8 @@ class SingleOrthonormalMatrixGenerationSystem:
                 vt = (c + s)*vt
                 vb = (c - s)*vb
                 vt = vt - u
-                if self.partial_difference and iAng == index_pd_angle:
-                    matrix = torch.zeros_like(matrix,dtype=self.dtype,requires_grad=False)
                 matrix[iBtm,:] = vb + u
                 iAng = iAng + 1
             matrix[iTop,:] = vt
-        matrix = mus.view(-1,1) * matrix
 
-        return matrix #.clone()
+        return matrix
