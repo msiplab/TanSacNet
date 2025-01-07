d7ba46bb299c76cf9c03f87ee73f8700156f984b
Shogo MURAMATSU <shogo@eng.niigata-u.ac.jp>
2025-01-07 10:10:36 +0900

    Prepared a new class LsunAnalysis2dNetwork < dlnetwork

---
+++ code/+tansacnet/+lsun/LsunAnalys2dNetwork.m
@@ -0,0 +1,37 @@
+classdef LsunAnalys2dNetwork < dlnetwork
+    %UNTITLED このクラスの概要をここに記述
+    %   詳細説明をここに記述
+
+    properties
+        % fcn_createlsunlgraph2d 参照
+    end
+
+    methods
+        function obj = LsunAnalysis2dNetwork(varargin)
+            %UNTITLED このクラスのインスタンスを作成
+            %   詳細説明をここに記述
+            
+            % fcn_createlsunlgraph2d のプロパティ設定を参照
+        end
+
+        function analyzerFactory = getAnalyzerFactory(net)
+            % ???
+            analyzerFactory = [];
+        end
+
+        function adjnet = transpose(obj)
+            %METHOD1 このメソッドの概要をここに記述
+            %   詳細説明をここに記述
+            
+            % fcn_cpparamsana2syn を参照
+        end
+    end
+
+    methods (Hidden=false)
+        function dlnet = dlnetwork(net)
+           % ???
+            dlnet = [];
+        end
+    end
+
+end
