21d5562bd573fa13bff38a5ce84fa9bbcf0dbb89
zhangchen001 <51438652+zhangchen001@users.noreply.github.com>
2026-07-06 17:32:00 +0900

    Update LSUN_POD_OnlineDMD_setup.m

--- code/examples/LSUN_POD_OnlineDMD/LSUN_POD_OnlineDMD_setup.m
+++ code/examples/LSUN_POD_OnlineDMD/LSUN_POD_OnlineDMD_setup.m
@@ -7,5 +7,5 @@ LSUN_POD_OnlineDMD_DIR = "LSUN_POD_OnlineDMD"+SAIVDR_VER;
 if ~exist(LSUN_POD_OnlineDMD_DIR,"dir")
     unzip("https://github.com/msiplab/LSUN_POD_OnlineDMD/archive/refs/tags/"+LSUN_POD_OnlineDMD_VER+".zip")
 else
-    disp(LSUN_POD_OnlineDMD_DIR+" exits.")
+    disp(LSUN_POD_OnlineDMD_DIR+" exists.")
 end
