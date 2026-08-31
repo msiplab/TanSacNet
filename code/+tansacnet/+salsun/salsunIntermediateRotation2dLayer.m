cd3873416565c3f82604ec03068e372a51b60b50
motoyasu0813 <motoyasu0813@icloud.com>
2026-08-31 14:41:45 +0900

    Implement salsunIntermediateRotation2dLayer

---
+++ code/+tansacnet/+salsun/salsunIntermediateRotation2dLayer.m
@@ -0,0 +1,193 @@
+classdef salsunIntermediateRotation2dLayer < tansacnet.lsun.lsunRotation2dLayerBase %#codegen
+    %SALSUNINTERMEDIATEROTATION2DLAYER
+    %
+    %   Data-path input  'x'     : nChsTotal x nRows x nCols x nSamples
+    %
+    %   Control-path input 'theta': nAngles x (nRows*nCols) x nSamples
+    %                               where nAngles = (nChsTotal-2)*nChsTotal/8
+    %                               (angles may vary from sample to
+    %                               sample, unlike tansacnet.lsun's
+    %                               Angles, which only vary block by
+    %                               block)
+    %
+    %   Output                   : nChsTotal x nRows x nCols x nSamples
+    %
+    % Requirements: MATLAB R2022a
+    %
+    % Copyright (c) 2026, Motoyasu SUZUKI and Shogo MURAMATSU
+    %
+    % All rights reserved.
+    %
+    % Contact address: Shogo MURAMATSU,
+    %                Faculty of Engineering, Niigata University,
+    %                8050 2-no-cho Ikarashi, Nishi-ku,
+    %                Niigata, 950-2181, JAPAN
+    %
+    % http://msiplab.eng.niigata-u.ac.jp/
+
+    properties
+        % (Optional) Layer properties.
+        Stride
+        Mode
+        NumberOfBlocks
+        Device
+        DType
+    end
+
+    properties (Dependent)
+        Mus
+    end
+
+    properties (Access = private)
+        PrivateNumberOfChannels
+        PrivateMus
+    end
+
+    methods
+        function layer = salsunIntermediateRotation2dLayer(varargin)
+            % (Optional) Create a myLayer.
+            % This function must have the same name as the class.
+            p = inputParser;
+            addParameter(p,'Stride',[])
+            addParameter(p,'Mus',[])
+            addParameter(p,'Mode','Synthesis')
+            addParameter(p,'Name','')
+            addParameter(p,'NumberOfBlocks',[1 1])
+            addParameter(p,'DType','double')
+            addParameter(p,'Device','cuda')
+            parse(p,varargin{:})
+
+            % Layer constructor function goes here.
+            layer.Stride = p.Results.Stride;
+            layer.NumberOfBlocks = p.Results.NumberOfBlocks;
+            layer.PrivateNumberOfChannels = [ceil(prod(layer.Stride)/2) floor(prod(layer.Stride)/2)];
+            layer.Name = p.Results.Name;
+            layer.Mode = p.Results.Mode;
+            layer.Mus = p.Results.Mus;
+            layer.Description = layer.Mode ...
+                + " SA-LSUN intermediate rotation (state-controlled) " ...
+                + "(ps,pa) = (" ...
+                + layer.PrivateNumberOfChannels(1) + "," ...
+                + layer.PrivateNumberOfChannels(2) + ")";
+            layer.Type = '';
+            layer.Device = p.Results.Device;
+            layer.DType = p.Results.DType;
+
+            % Two named inputs: data path 'x' and control path 'theta'.
+            layer.InputNames = { 'x', 'theta' };
+        end
+
+        function Z = predict(layer, X, Theta)
+            % Forward input data through the layer at prediction time and
+            % output the result.
+            %
+            % Inputs:
+            %         layer - Layer to forward propagate through
+            %         X     - Data-path input
+            %         Theta - Control-path input (rotation angles)
+            % Outputs:
+            %         Z     - Output of layer forward function
+
+            nrows = size(X,2);
+            ncols = size(X,3);
+            nSamples = size(X,4);
+            ps = layer.PrivateNumberOfChannels(1);
+            pa = layer.PrivateNumberOfChannels(2);
+            %
+            if ~strcmp(layer.Mode,'Analysis') && ~strcmp(layer.Mode,'Synthesis')
+                throw(MException('NsoltLayer:InvalidMode',...
+                    '%s : Mode should be either of Synthesis or Analysis',...
+                    layer.Mode))
+            end
+            isAnalysis = strcmp(layer.Mode,'Analysis');
+            musU = cast(layer.PrivateMus,'like',Theta);
+
+            Y = X;
+            Ya = reshape(Y(ps+1:ps+pa,:,:,:),pa,nrows*ncols,nSamples);
+            Za = zeros(pa,nrows*ncols,nSamples,'like',Y);
+            for iSample = 1:nSamples
+                anglesU = Theta(:,:,iSample);
+                fcn_orthmtxgen = tansacnet.lsun.get_fcn_orthmtxgen(anglesU);
+                Un_i = fcn_orthmtxgen(anglesU,musU);
+                if isAnalysis
+                    A_ = Un_i;
+                else
+                    A_ = permute(Un_i,[2 1 3]);
+                end
+                Za(:,:,iSample) = layer.applyBlockwiseMatrix(A_, Ya(:,:,iSample));
+            end
+            Y(ps+1:ps+pa,:,:,:) = reshape(Za,pa,nrows,ncols,nSamples);
+            Z = Y;
+        end
+
+        function [dLdX, dLdTheta] = backward(layer, X, Theta, ~, dLdZ, ~)
+            % (Optional) Backward propagate the derivative of the loss
+            % function through the layer.
+            %
+            % Inputs:
+            %         layer       - Layer to backward propagate through
+            %         X, Theta    - Layer inputs (data path, control path)
+            %         Z           - Output of layer forward function
+            %         dLdZ        - Gradient propagated from the next layer
+            %         memory      - Memory value from forward function
+            % Outputs:
+            %         dLdX        - Derivative of the loss w.r.t. X
+            %         dLdTheta    - Derivative of the loss w.r.t. Theta
+            %                       (fed back into the control-path
+            %                       parameter estimator)
+
+            nrows = size(dLdZ,2);
+            ncols = size(dLdZ,3);
+            nSamples = size(dLdZ,4);
+            ps = layer.PrivateNumberOfChannels(1);
+            pa = layer.PrivateNumberOfChannels(2);
+            nBlks = nrows*ncols;
+            %
+            isAnalysis = strcmp(layer.Mode,'Analysis');
+            musU = cast(layer.PrivateMus,'like',Theta);
+
+            % dLdX = dZdX x dLdZ
+            dLdX = reshape(dLdZ,ps+pa,nrows,ncols,nSamples);
+            cdLd_low = reshape(dLdX(ps+1:ps+pa,:,:,:),pa,nBlks,nSamples);
+            dldz_low = reshape(dLdZ(ps+1:ps+pa,:,:,:),pa,nBlks,nSamples);
+            c_low = reshape(X(ps+1:ps+pa,:,:,:),pa,nBlks,nSamples);
+            nAngles = size(Theta,1);
+            dLdTheta = zeros(nAngles,nBlks,nSamples,'like',dLdZ);
+            for iSample = 1:nSamples
+                anglesU = Theta(:,:,iSample);
+                fcn_orthmtxgen = tansacnet.lsun.get_fcn_orthmtxgen(anglesU);
+                Un_i = fcn_orthmtxgen(anglesU,musU);
+                if isAnalysis
+                    A_ = permute(Un_i,[2 1 3]);
+                else
+                    A_ = Un_i;
+                end
+                cdLd_low(:,:,iSample) = layer.applyBlockwiseMatrix(A_, cdLd_low(:,:,iSample));
+
+                % dLdTheta_i = <dLdZ,(dVdTheta_i)X>
+                dLdTheta(:,:,iSample) = layer.computeAngleGradient( ...
+                    Un_i, anglesU, musU, ...
+                    c_low(:,:,iSample), dldz_low(:,:,iSample));
+            end
+            dLdX(ps+1:ps+pa,:,:,:) = reshape(cdLd_low,pa,nrows,ncols,nSamples);
+        end
+
+        function mus = get.Mus(layer)
+            mus = layer.PrivateMus;
+        end
+
+        function layer = set.Mus(layer,mus)
+            nBlocks = prod(layer.NumberOfBlocks);
+            pa = layer.PrivateNumberOfChannels(2);
+            if isempty(mus)
+                mus = ones(pa,nBlocks);
+            elseif isscalar(mus)
+                mus = mus*ones(pa,nBlocks,'like',mus);
+            end
+            %
+            layer.PrivateMus = mus;
+        end
+
+    end
+
+end
