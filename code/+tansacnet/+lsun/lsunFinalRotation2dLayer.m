d53e88bae08835e6833c2a5ab8b34d516db59ea7
motoyasu0813 <motoyasu0813@icloud.com>
2026-09-02 15:54:05 +0900

    Changed the way the blockwise matrix is applied to the input state in lsunFinalRotation2dLayer.m.

--- code/+tansacnet/+lsun/lsunFinalRotation2dLayer.m
+++ code/+tansacnet/+lsun/lsunFinalRotation2dLayer.m
@@ -1,4 +1,4 @@
-classdef lsunFinalRotation2dLayer < nnet.layer.Layer %#codegen
+classdef lsunFinalRotation2dLayer < tansacnet.lsun.lsunRotation2dLayerBase %#codegen
     %LSUNFINALROTATION2DLAYER
     %
     %   コンポーネント別に入力(nComponents):
@@ -53,7 +53,15 @@ classdef lsunFinalRotation2dLayer < nnet.layer.Layer %#codegen
         W0T
         U0T
     end
-    
+
+    properties (Hidden, Constant)
+        % This layer always applies the transposed (synthesis-side)
+        % matrices W0'/U0' (see predict), so this constant lets the
+        % inherited, unmodified lsunRotation2dLayerBase.computeAngleGradient
+        % (which branches on layer.Mode) be reused as-is.
+        Mode = 'Synthesis'
+    end
+
     methods
         function layer = lsunFinalRotation2dLayer(varargin)
             % (Optional) Create a myLayer.
@@ -110,7 +118,7 @@ classdef lsunFinalRotation2dLayer < nnet.layer.Layer %#codegen
             % Layer forward function for prediction goes here.
             
             nrows = size(X,2);
-            ncols = size(X,3);    
+            ncols = size(X,3);
             nSamples = size(X,4);
             ps = layer.PrivateNumberOfChannels(1);
             pa = layer.PrivateNumberOfChannels(2);
@@ -120,31 +128,12 @@ classdef lsunFinalRotation2dLayer < nnet.layer.Layer %#codegen
             %
             W0T_ = layer.W0T;
             U0T_ = layer.U0T;
-            %Y = X; %permute(X,[3 1 2 4]);
             Y = reshape(X,ps+pa,nrows*ncols,nSamples);
-            %Ys = reshape(X(1:ps,:,:,:),ps,nrows*ncols,nSamples);
-            %Ya = reshape(X(ps+1:ps+pa,:,:,:),pa,nrows*ncols,nSamples);
-            Zs = zeros(ps,nrows*ncols,nSamples,'like',Y);
-            Za = zeros(pa,nrows*ncols,nSamples,'like',Y);
-            for iSample = 1:nSamples
-                if isgpuarray(X)
-                    Ys_iSample = permute(Y(1:ps,:,iSample),[1 4 2 3]);
-                    Ya_iSample = permute(Y(ps+1:end,:,iSample),[1 4 2 3]);
-                    Zs_iSample = pagefun(@mtimes,W0T_,Ys_iSample);
-                    Za_iSample = pagefun(@mtimes,U0T_,Ya_iSample);
-                    Zs(:,:,iSample) = ipermute(Zs_iSample,[1 4 2 3]);
-                    Za(:,:,iSample) = ipermute(Za_iSample,[1 4 2 3]);
-                else
-                    for iblk = 1:(nrows*ncols)
-                        Zs(:,iblk,iSample) = W0T_(1:ps,:,iblk)*Y(1:ps,iblk,iSample);
-                        Za(:,iblk,iSample) = U0T_(1:pa,:,iblk)*Y(ps+1:end,iblk,iSample);
-                    end
-                end
-            end
-            Zsa = cat(1,Zs,Za);
-            %Z = ipermute(reshape(Zsa,nDecs,nrows,ncols,nSamples),...
-            %    [3 1 2 4]);
-            Z = reshape(Zsa,ps+pa,nrows,ncols,nSamples);
+            Ys = Y(1:ps,:,:);
+            Ya = Y(ps+1:end,:,:);
+            Zs = layer.applyBlockwiseMatrix(W0T_(1:ps,:,:),Ys);
+            Za = layer.applyBlockwiseMatrix(U0T_(1:pa,:,:),Ya);
+            Z = reshape([Zs;Za],ps+pa,nrows,ncols,nSamples);
 
         end
         
@@ -163,116 +152,42 @@ classdef lsunFinalRotation2dLayer < nnet.layer.Layer %#codegen
             %                             inputs
             %         dLdW1, ..., dLdWk - Derivatives of the loss with respect to each
             %                             learnable parameter
-            %import tansacnet.lsun.get_fcn_orthmtxgen_diff
-            
             nrows = size(dLdZ,2);
-            ncols = size(dLdZ,3);            
+            ncols = size(dLdZ,3);
             nSamples = size(dLdZ,4);
             ps = layer.PrivateNumberOfChannels(1);
             pa = layer.PrivateNumberOfChannels(2);
-            %{
-            if size(layer.PrivateAngles,2) == 1
-                layer.Angles = repmat(layer.PrivateAngles,[1 (nrows*ncols)]);
-            end
-            if size(layer.PrivateMus,2) == 1
-                layer.Mus = repmat(layer.PrivateMus,[1 (nrows*ncols)]);
-            end
-            %}
             if layer.isUpdateRequested
                 layer = layer.updateParameters();
             end
             angles = layer.PrivateAngles;
             nAngles = size(angles,1);
-            mus = cast(layer.Mus,'like',angles);                        
+            mus = cast(layer.Mus,'like',angles);
             muW = mus(1:ps,:);
             muU = mus(ps+1:end,:);
             anglesW = angles(1:nAngles/2,:);
             anglesU = angles(nAngles/2+1:end,:);
-            %W0 = fcn_orthmtxgen(anglesW,muW,0);
-            %U0 = fcn_orthmtxgen(anglesU,muU,0);
-            %[W0,dW0Pst,dW0Pre] = fcn_orthmtxgen_diff(anglesW,muW,0,[],[]);            
-            %[U0,dU0Pst,dU0Pre] = fcn_orthmtxgen_diff(anglesU,muU,0,[],[]);            
-            W0_T = layer.W0T; %transpose(fcn_orthmtxgen(anglesW,muW,0));
-            U0_T = layer.U0T; %transpose(fcn_orthmtxgen(anglesU,muU,0));
-            W0 = permute(W0_T,[2 1 3]);
-            U0 = permute(U0_T,[2 1 3]);
-            dW0Pst = zeros(size(W0),'like',W0);
-            dU0Pst = zeros(size(U0),'like',U0);
-            for iblk = 1:(nrows*ncols)
-                dW0Pst(:,:,iblk) = bsxfun(@times,muW(:,iblk),W0(:,:,iblk));
-                dU0Pst(:,:,iblk) = bsxfun(@times,muU(:,iblk),U0(:,:,iblk));
-            end
-            dW0Pre = repmat(eye(ps,'like',W0),[1 1 (nrows*ncols)]);
-            dU0Pre = repmat(eye(pa,'like',U0),[1 1 (nrows*ncols)]);
-            
-            % Layer backward function goes here.
-            % dLdX = dZdX x dLdZ
-            %adldz_ = dLdZ; %permute(dLdZ,[3 1 2 4]);
-            %cdLd_ = reshape(adldz_,ps+pa,nrows*ncols,nSamples);
-            cdLd_ = reshape(dLdZ,ps+pa,nrows*ncols,nSamples);
-            cdLd_upp = zeros(ps,nrows*ncols,nSamples,'like',cdLd_);
-            cdLd_low = zeros(pa,nrows*ncols,nSamples,'like',cdLd_);
-            for iSample = 1:nSamples
-                if isgpuarray(X)
-                    cdLd_upp_iSample = permute(cdLd_(1:ps,:,iSample),[1 4 2 3]);
-                    cdLd_low_iSample = permute(cdLd_(ps+1:end,:,iSample),[1 4 2 3]);
-                    cdLd_upp_iSample = pagefun(@mtimes,W0(:,1:ps,:),cdLd_upp_iSample);
-                    cdLd_low_iSample = pagefun(@mtimes,U0(:,1:pa,:),cdLd_low_iSample);
-                    cdLd_upp(:,:,iSample) = ipermute(cdLd_upp_iSample,[1 4 2 3]);
-                    cdLd_low(:,:,iSample) = ipermute(cdLd_low_iSample,[1 4 2 3]);
-                else
-                    for iblk = 1:(nrows*ncols)
-                        cdLd_upp(:,iblk,iSample) = W0(:,1:ps,iblk)*cdLd_(1:ps,iblk,iSample);
-                        cdLd_low(:,iblk,iSample) = U0(:,1:pa,iblk)*cdLd_(ps+1:ps+pa,iblk,iSample);
-                    end
-                end
-            end
-            %adLd_ = reshape(cat(1,cdLd_upp,cdLd_low),pa+ps,nrows,ncols,nSamples);
-            %dLdX = adLd_; %ipermute(adLd_,[3 1 2 4]);
-            dLdX = reshape(cat(1,cdLd_upp,cdLd_low),pa+ps,nrows,ncols,nSamples);
-            
+            W0T_ = layer.W0T;
+            U0T_ = layer.U0T;
+            W0_ = permute(W0T_,[2 1 3]);
+            U0_ = permute(U0T_,[2 1 3]);
+
+            % dLdX = dZdX x dLdZ (predict applied W0'/U0', so backward
+            % applies (W0')' = W0 / (U0')' = U0, untransposed).
+            Ys = reshape(dLdZ(1:ps,:,:,:),ps,nrows*ncols,nSamples);
+            Ya = reshape(dLdZ(ps+1:ps+pa,:,:,:),pa,nrows*ncols,nSamples);
+            Ys = layer.applyBlockwiseMatrix(W0_(:,1:ps,:),Ys);
+            Ya = layer.applyBlockwiseMatrix(U0_(:,1:pa,:),Ya);
+            dLdX = reshape(cat(1,Ys,Ya),ps+pa,nrows,ncols,nSamples);
+
             % dLdWi = <dLdZ,(dVdWi)X>
-            fcn_orthmtxgen_diff = tansacnet.lsun.get_fcn_orthmtxgen_diff(angles);            
-            dLdW = zeros(nAngles,nrows*ncols,'like',dLdZ);
-            %dldz_ = dLdZ; %permute(dLdZ,[3 1 2 4]);
             dldz_upp = reshape(dLdZ(1:ps,:,:,:),ps,nrows*ncols,nSamples);
             dldz_low = reshape(dLdZ(ps+1:ps+pa,:,:,:),pa,nrows*ncols,nSamples);
-            %a_ = X; %permute(X,[3 1 2 4]);
             c_upp = reshape(X(1:ps,:,:,:),ps,nrows*ncols,nSamples);
             c_low = reshape(X(ps+1:ps+pa,:,:,:),pa,nrows*ncols,nSamples);
-            for iAngle = uint32(1:nAngles/2)
-                %dW0_T = transpose(fcn_orthmtxgen(anglesW,muW,iAngle));
-                %dU0_T = transpose(fcn_orthmtxgen(anglesU,muU,iAngle));
-                [dW0,dW0Pst,dW0Pre] = fcn_orthmtxgen_diff(anglesW,muW,iAngle,dW0Pst,dW0Pre);
-                [dU0,dU0Pst,dU0Pre] = fcn_orthmtxgen_diff(anglesU,muU,iAngle,dU0Pst,dU0Pre);
-                dW0_T = permute(dW0,[2 1 3]);
-                dU0_T = permute(dU0,[2 1 3]);
-                if isgpuarray(X)
-                    c_upp_ext = permute(c_upp,[1 4 2 3]); % idx 1 iblk iSample
-                    c_low_ext = permute(c_low,[1 4 2 3]); % idx 1 iblk iSample
-                    d_upp_ext = pagefun(@mtimes,dW0_T(1:ps,:,:),c_upp_ext); % idx 1 iblk iSample
-                    d_low_ext = pagefun(@mtimes,dU0_T(1:pa,:,:),c_low_ext); % idx 1 iblk iSample
-                    d_upp = ipermute(d_upp_ext,[1 4 2 3]);
-                    d_low = ipermute(d_low_ext,[1 4 2 3]);
-                    dLdW(iAngle,:) = sum(bsxfun(@times,dldz_upp,d_upp),[1 3]);
-                    dLdW(nAngles/2+iAngle,:) = sum(bsxfun(@times,dldz_low,d_low),[1 3]);
-                else
-                    for iblk = 1:(nrows*ncols)
-                        dldz_upp_iblk = squeeze(dldz_upp(:,iblk,:));
-                        dldz_low_iblk = squeeze(dldz_low(:,iblk,:));
-                        c_upp_iblk = squeeze(c_upp(:,iblk,:));
-                        c_low_iblk = squeeze(c_low(:,iblk,:));
-                        d_upp_iblk = zeros(size(c_upp_iblk),'like',c_upp_iblk);
-                        d_low_iblk = zeros(size(c_low_iblk),'like',c_low_iblk);
-                        for iSample = 1:nSamples
-                            d_upp_iblk(:,iSample) = dW0_T(1:ps,:,iblk)*c_upp_iblk(:,iSample);
-                            d_low_iblk(:,iSample) = dU0_T(1:pa,:,iblk)*c_low_iblk(:,iSample);
-                        end
-                        dLdW(iAngle,iblk) = sum(bsxfun(@times,dldz_upp_iblk,d_upp_iblk),'all');
-                        dLdW(nAngles/2+iAngle,iblk) = sum(bsxfun(@times,dldz_low_iblk,d_low_iblk),'all');
-                    end
-                end
-            end
+            dLdW_upp = layer.computeAngleGradient(W0_,anglesW,muW,c_upp,dldz_upp);
+            dLdW_low = layer.computeAngleGradient(U0_,anglesU,muU,c_low,dldz_low);
+            dLdW = cat(1,dLdW_upp,dLdW_low);
         end
         
         function nodcleak = get.NoDcLeakage(layer)
