23e2b4f915e4188e703738dd37b1b394fc6611fd
motoyasu0813 <motoyasu0813@icloud.com>
2026-09-07 13:29:42 +0900

    Add salsunStateStandardization2dLayer and its test case

---
+++ code/+tansacnet/+testcase/+salsun/salsunStateStandardization2dLayerTestCase.m
@@ -0,0 +1,74 @@
+classdef salsunStateStandardization2dLayerTestCase < matlab.unittest.TestCase
+    %SALSUNSTATESTANDARDIZATION2DLAYERTESTCASE
+    %
+    %   Input  'in'  : nFeat x nRows x nCols x nSamples
+    %   Output 'out' : nFeat x nRows x nCols x nSamples
+    %
+    % Requirements: MATLAB R2022a
+    %
+    % Copyright (c) 2026, Motoyasu SUZUKI and Shogo MURAMATSU
+    %
+    % All rights reserved.
+    %
+    % Contact address: Shogo MURAMATSU,
+    %                Faculty of Engineering, Niigata University,
+    %                8050 2-no-cho Ikarashi, Nishi-ku,
+    %                Niigata, 950-2181, JAPAN
+    %
+    % http://msiplab.eng.niigata-u.ac.jp/
+
+    methods (Test)
+
+        function testPredictZeroMeanUnitVariance(testCase)
+            import matlab.unittest.constraints.IsEqualTo
+            import matlab.unittest.constraints.AbsoluteTolerance
+            tolObj = AbsoluteTolerance(1e-6);
+            import tansacnet.salsun.*
+
+            nFeat = 5;
+            nRows = 6;
+            nCols = 7;
+            nSamples = 3;
+            X = 10*randn(nFeat,nRows,nCols,nSamples) + 100;
+
+            layer = salsunStateStandardization2dLayer('Name','Std');
+            Z = layer.predict(X);
+
+            testCase.verifySize(Z,size(X));
+            nBlks = nRows*nCols;
+            actualMean = mean(Z,[2 3]);
+            actualVar = sum((Z-actualMean).^2,[2 3])/(nBlks-1);
+            testCase.verifyThat(actualMean,IsEqualTo(zeros(nFeat,1,1,nSamples),'Within',tolObj));
+            testCase.verifyThat(actualVar,IsEqualTo(ones(nFeat,1,1,nSamples),'Within',AbsoluteTolerance(1e-4)));
+        end
+
+        function testPredictMatchesReference(testCase)
+            import matlab.unittest.constraints.IsEqualTo
+            import matlab.unittest.constraints.AbsoluteTolerance
+            tolObj = AbsoluteTolerance(1e-10);
+            import tansacnet.salsun.*
+
+            nFeat = 4;
+            nRows = 3;
+            nCols = 3;
+            nSamples = 2;
+            X = randn(nFeat,nRows,nCols,nSamples);
+
+            layer = salsunStateStandardization2dLayer('Name','Std');
+            actualZ = layer.predict(X);
+
+            % Reference computed by reshaping to (nFeat, nBlks, nSamples)
+            % as in the original SA_LSUN.mlx script.
+            nBlks = nRows*nCols;
+            Xr = reshape(permute(X,[1 2 3 4]),nFeat,nBlks,nSamples); %#ok<NASGU>
+            X_ = reshape(X,nFeat,nBlks,nSamples);
+            mu = mean(X_,2);
+            sigma = std(X_,0,2) + 1e-8;
+            expctdZ = reshape((X_-mu)./sigma,nFeat,nRows,nCols,nSamples);
+
+            testCase.verifyThat(actualZ,IsEqualTo(expctdZ,'Within',tolObj));
+        end
+
+    end
+
+end
